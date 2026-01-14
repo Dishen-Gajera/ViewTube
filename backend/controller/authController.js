3a4b5f385ff94131049210967c88f283a9e25a75
Dishen-Gajera <dishengajera@gmail.com>
2026-01-14 18:32:18 +0530

    site spelling change

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -34,7 +34,7 @@ export const signUP = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      samesite: "None",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -61,7 +61,7 @@ export const signIn = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      samesite: "None",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -103,7 +103,7 @@ export const googleAuth = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      samesite: "None",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
